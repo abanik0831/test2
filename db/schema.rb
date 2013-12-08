10c72c0685b7ed9c46277af23228bf207d1e65b5
Arnab Banik <abanik@gmail.com>
2013-12-08 04:10:26 -0800

    username admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207005752) do
+ActiveRecord::Schema.define(version: 20131208120925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20131207005752) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "admin_user"
+    t.string   "username"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
