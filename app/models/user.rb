f0e1d7ca70618066d06cac443d1063d195c05987
Arnab Banik <abanik@gmail.com>
2013-12-31 17:14:42 -0800

    valid username3

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :profile_attributes
 
   validates :email, :email => true
-  validates :username, :presence =>  true
+  validates :username, :presence =>  true, :uniqueness => true
 
 
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy, :autosave => true
