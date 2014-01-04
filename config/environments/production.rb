79d81ccac000e37e0744be87e41e92e8dbac78d4
Arnab Banik <abanik@gmail.com>
2014-01-04 03:31:58 -0800

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ StartupSocial::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  config.action_mailer.default_url_options = { :host => 'startupsocialmvp.heroku.com' }
+
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
